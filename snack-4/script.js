28a3398f12da8cebedf91cd307a05a1309164339
bigazzi96 <fbigazzi96@gmail.com>
2024-05-13 12:24:54 +0200

    added js

---
+++ snack-4/script.js
@@ -0,0 +1,23 @@
+//*In un array sono contenuti i nomi degli invitati alla festa del grande Gatsby, chiedi all'utente il suo nome e comunicagli se può partecipare o no alla festa.
+
+const festa= [nick,john,kevin, jim,frank];
+
+
+
+const username = prompt ('Inserisci il tuo nome:');
+console.log(username);
+
+if (guestList.includes(username)){
+console.log('you can join us!')
+}
+
+else(guestList.includes(username)){
+
+    console.log(plese, get back to  your home)
+
+   console.log
+})
+
+
+
+
