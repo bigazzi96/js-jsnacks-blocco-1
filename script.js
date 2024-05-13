657682c317641bed3579dab79ed37c5355a3247e
bigazzi96 <fbigazzi96@gmail.com>
2024-05-13 11:56:16 +0200

    added js

---
+++ script.js
@@ -0,0 +1,19 @@
+//*L'utente inserisce due numeri in successione, con due prompt.
+//*Il software stampa il maggiore
+
+
+const firstNumber = prompt parseInt ('inserisci numero 1')10,);
+const secondnumber = number.parse.int(prompt ('inserisci numero 2')10,)
+
+if (num1 > num2){
+    console.log(firstnumber);
+}
+
+else if{ (firstNumber < secondnumber)
+console.log(secondnumber);
+}
+
+
+console.log(Number);
+
+
