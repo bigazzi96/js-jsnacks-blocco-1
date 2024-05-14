319b891e077e61aa8a974c2841285d7a0e6d0213
bigazzi96 <fbigazzi96@gmail.com>
2024-05-14 10:54:09 +0200

    snack 6

--- snack-4/script.js
+++ snack-4/script.js
@@ -1,5 +1,12 @@
 //*In un array sono contenuti i nomi degli invitati alla festa del grande Gatsby, chiedi all'utente il suo nome e comunicagli se può partecipare o no alla festa.
 
+for(let index=0;index < 6; index++){
+    const usernumber = parseint(promt('type a number'),10);
+    if{(numero % 2 === 1);
+        numberArray.push(userNumber);
+        
+}
+
 const festa= [nick,john,kevin, jim,frank];
 
 
---
+++ snack-5/script.js
@@ -0,0 +1,19 @@
+//Crea un array vuoto.
+ //Chiedi per 6 volte all'utente di inserire un numero,
+ //se è dispari inseriscilo nell'array.
+
+
+ const number = [];
+ 
+ console.log (number);
+
+ const usernumber = number parseInt (from(type a number),10);
+
+
+number.push();
+ console.log(number);
+ 
+
+
+ if{(numero % 2 === 1)
+	console.log(number)};
